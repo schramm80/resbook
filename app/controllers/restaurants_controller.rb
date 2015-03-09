3a33eb4b44e5d67b1d4b0f81e040fdc9d520ae44
Schramm <carlos.schramm80@gmail.com>
2015-03-09 01:18:56 +0100

    add views and functionality to the app

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,44 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurant = Restaurant.all
   end
 
   def show
+     @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(params[:restaurants])
+ 
+      if @restaurant.save
+        redirect_to restaurants_path, :notice => "Restaurant saved!"
+      else
+        render "new"
+      end
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+	
+    	if @restaurant.update_attributes(params[:restaurant]) 
+    		redirect_to restaurants_path, :notice => "The restaurant's info has been actualized"
+    	else
+    		render "edit"
+    	end
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    	@restaurant.destroy
+    	redirect_to restaurants_path, :notice => "Your restaurant has been deleted"
+     
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ActiveRecord::Base
+  
+  validates :name, :description, :address, :phone, :presence => true
+  validates :name, :length => { :minimum => 2 }
+  validates :phone, :length => { :minimum => 9 }
+  validates :name, :uniqueness => { :message => "This name is already taken" }
 end
